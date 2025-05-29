4a61d1e690f59fffec72d6173d25b7989f6c8fb7
soseoyo12 <songsy0612@gmail.com>
2025-05-29 19:12:38 +0900

    Title: 특정 수 맞히기, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 250529/특정 수 맞히기/catching-specific-number.py
+++ 250529/특정 수 맞히기/catching-specific-number.py
@@ -1,9 +1,9 @@
 while True:
     n=int(input())
     if n > 25:
-        print("Higher")
-    elif n < 25:
         print("Lower")
+    elif n < 25:
+        print("Higher")
     else:
         print("Good")
-    n=0
+        break
