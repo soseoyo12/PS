74cf7ee6496ef5e59d2630522ea5df97c1c004f0
soseoyo12 <songsy0612@gmail.com>
2025-05-30 12:56:33 +0900

    Title: 정상적인 구구단, Time: 74ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 250530/정상적인 구구단/normal-multiple.py
@@ -0,0 +1,13 @@
+N=int(input())
+
+for i in range(1,N+1):
+    for ii in range(1,N+1):
+        if ii == N:
+            print(f"{i} * {ii} = {i*ii}")
+            break
+        print(f"{i} * {ii} = {i*ii}", end=", ")
+
+
+
+
+x
