3a28d1cbcb2869888e406008f3eb1763da93ccdc
SeongYong_Song <songsy0612@gmail.com>
2025-07-09 15:24:11 +0900

    [Bronze V] Title: 개, Time: 8 ms, Memory: 79504 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  swift_Algorithm
+//
+//  Created by SeongYongSong on 7/8/25.
+//
+
+import Foundation
+
+print("|\\_/|")
+print("|q p|   /}")
+print("( 0 )\"\"\"\\")
+print("|\"^\"`    |")
+print("||_/=\\\\__|")
+
+
