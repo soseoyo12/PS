ca2b84a59514f537bd0555a22aafddcfb8de12f2
SeongYong_Song <songsy0612@gmail.com>
2025-07-09 15:35:26 +0900

    [Bronze V] Title: A+B, Time: 8 ms, Memory: 79512 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  swift_Algorithm
+//
+//  Created by SeongYongSong on 7/8/25.
+//
+
+import Foundation
+
+let numbers = readLine()!.split(separator: " ").map { Int($0)! }
+let a = numbers[0]
+let b = numbers[1]
+
+print("\(a+b)")
+
