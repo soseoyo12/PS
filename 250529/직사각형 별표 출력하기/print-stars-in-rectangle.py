c0a91419d453c6f0501ad4dbcdaeb9a4f780c1d7
SeongYong_Song <songsy0612@gmail.com>
2025-07-09 14:28:05 +0900

    Delete 250529 directory

--- 250529/직사각형 별표 출력하기/print-stars-in-rectangle.py
+++
@@ -1,6 +0,0 @@
-N,M=map(int,input().split())
-
-for i in range(N):
-    for ii in range(M):
-        print("*", end=" ")
-    print()
--- 250529/특정 수 맞히기/catching-specific-number.py
+++
@@ -1,9 +0,0 @@
-while True:
-    n=int(input())
-    if n > 25:
-        print("Lower")
-    elif n < 25:
-        print("Higher")
-    else:
-        print("Good")
-        break
