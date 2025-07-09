30d926bce970cbe89f7bc4441f2ac3aa025ae11d
SeongYong_Song <songsy0612@gmail.com>
2025-07-09 15:21:11 +0900

    [Bronze V] Title: 고양이, Time: 8 ms, Memory: 79504 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  swift_Algorithm
+//
+//  Created by SeongYongSong on 7/8/25.
+//
+
+import Foundation
+
+print("\\    /\\")
+print(" )  ( \')")
+print("(  /  )")
+print(" \\(__)|")
+
