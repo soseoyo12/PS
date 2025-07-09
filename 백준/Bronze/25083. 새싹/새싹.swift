bb506446221683675f71ef1c837a5d95f0a38ef0
SeongYong_Song <songsy0612@gmail.com>
2025-07-09 15:27:30 +0900

    [Bronze V] Title: 새싹, Time: 8 ms, Memory: 79504 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  swift_Algorithm
+//
+//  Created by SeongYongSong on 7/8/25.
+//
+
+import Foundation
+
+print("         ,r\'\"7")
+print("r`-_   ,\'  ,/")
+print(" \\. \". L_r\'")
+print("   `~\\/")
+print("      |")
+print("      |")
+
+
+
